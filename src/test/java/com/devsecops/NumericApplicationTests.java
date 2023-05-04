240e4bd9fbd0533e9057a537f79068afe9f59c55
TaskMasterErnest <kluernest08@gmail.com>
2023-05-04 11:37:17 +0000

    modified the welcomeMessage test

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -46,11 +46,11 @@ public class NumericApplicationTests {
                 .andExpect(content().string("Greater than 50"));
     }
     
-    @Test
+@Test
     public void welcomeMessage() throws Exception {
-         this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-            .andExpect(content().string("Kubernetes DevsecOps"));
-    }
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+            .andExpect(content().string("Kubernetes DevSecOps"));
+}
     
 
 }
