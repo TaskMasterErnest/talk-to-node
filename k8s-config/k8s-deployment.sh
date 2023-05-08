653076a3bc80d583a5961eb2086492f661a2c807
TaskMasterErnest <kluernest08@gmail.com>
2023-05-08 17:18:32 +0000

    modified the Kubernetes configuration

--- k8s-config/k8s-deployment.sh
+++ k8s-config/k8s-deployment.sh
@@ -3,14 +3,14 @@
 #Kubernetes deployment script
 
 #first get the image to be used for the deployment
-sed -i "s#replace#${imageName}#g" k8s_deployment_service.yaml
+sed -i "s#replace#${imageName}#g" k8s-config/k8s_deployment_service.yaml
 
 kubectl -n default get deployment ${deploymentName} > /dev/null
 exit_code = "$?"
 
 if [[ "${exit_code}" -ne 0 ]]; then
   echo "deployment ${deploymentName} does NOT exist"
-  kubectl -n default apply -f k8s_deployment_service.yaml
+  kubectl -n default apply -f k8s-config/k8s_deployment_service.yaml
 else
   #here we confirm if the deployment exists. we just change the image build used for the file
   echo "deployment ${deploymentName} does exist"
