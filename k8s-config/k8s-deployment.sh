6f5862391ba49e3950b8ddb37e253d8890d760a8
TaskMasterErnest <kluernest08@gmail.com>
2023-05-08 17:39:07 +0000

    added Kubernetes deployment file for DEV environment; testing the replaceability of Pods

--- k8s-config/k8s-deployment.sh
+++ k8s-config/k8s-deployment.sh
@@ -5,17 +5,17 @@
 #first get the image to be used for the deployment
 sed -i "s#replace#${imageName}#g" k8s-config/k8s_deployment_service.yaml
 
-# kubectl -n default get deploy ${deploymentName} > /dev/null
-# exit_code = "$?"
+kubectl -n default get deploy ${deploymentName} > /dev/null
+exit_code = "$?"
 
-# if [[ "${exit_code}" -ne 0 ]]; then
-#   echo "deployment ${deploymentName} does NOT exist"
-#   kubectl -n default apply -f k8s-config/k8s_deployment_service.yaml
-# else
-#   #here we confirm if the deployment exists. we just change the image build used for the file
-#   echo "deployment ${deploymentName} does exist"
-#   echo "image name = ${imageName}"
-#   kubectl -n default set image deploy ${deploymentName} ${containerName}=${imageName} --record=true
-# fi
+if [[ "${exit_code}" -ne 0 ]]; then
+  echo "deployment ${deploymentName} does NOT exist"
+  kubectl -n default apply -f k8s-config/k8s_deployment_service.yaml
+else
+  #here we confirm if the deployment exists. we just change the image build used for the file
+  echo "deployment ${deploymentName} does exist"
+  echo "image name = ${imageName}"
+  kubectl -n default set image deploy ${deploymentName} ${containerName}=${imageName} --record=true
+fi
 
-kubectl apply -n default -f k8s-config/k8s_deployment_service.yaml
+# kubectl apply -n default -f k8s-config/k8s_deployment_service.yaml
