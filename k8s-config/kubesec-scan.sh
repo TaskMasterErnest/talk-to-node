40e37166631ce9a44fcb2242fae85b35876512da
TaskMasterErnest <kluernest08@gmail.com>
2023-05-10 15:18:11 +0000

    added new method for kubesec scan

--- k8s-config/kubesec-scan.sh
+++ k8s-config/kubesec-scan.sh
@@ -2,9 +2,15 @@
 
 #using the kubesec v2 API
 
-scan_result=$(curl -sSX POST --data-binary@"k8s-config/k8s_deployment_service.yaml" https://v2.kubesec.io/scan)
-scan_message=$(curl -sSX POST --data-binary@"k8s-config/k8s_deployment_service.yaml" https://v2.kubesec.io/scan | jq.[0].message -r)
-scan_score=$(curl -sSX POST --data-binary@"k8s-config/k8s_deployment_service.yaml" https://v2.kubesec.io/scan | jq.[0].score)
+# scan_result=$(curl -sSX POST --data-binary@"k8s-config/k8s_deployment_service.yaml" https://v2.kubesec.io/scan)
+# scan_message=$(curl -sSX POST --data-binary@"k8s-config/k8s_deployment_service.yaml" https://v2.kubesec.io/scan | jq.[0].message -r)
+# scan_score=$(curl -sSX POST --data-binary@"k8s-config/k8s_deployment_service.yaml" https://v2.kubesec.io/scan | jq.[0].score)
+
+# using kubesec docker image for scanning
+scan_result=$(docker run -i kubesec/kubesec:v2 scan /dev/stdin < k8s-config/k8s_deployment_service.yaml)
+scan_message=$(docker run -i kubesec/kubesec:v2 scan /dev/stdin < k8s-config/k8s_deployment_service.yaml | jq .[].message -r)
+scan_score=$(docker run -i kubesec/kubesec:v2 scan /dev/stdin < k8s-config/k8s_deployment_service.yaml | jq .[].score)
+
 
 # processing the results
 if [[ "${scan_score}" -ge 5 ]]; then
@@ -14,4 +20,4 @@ else
   echo "Score is $scan_score, which is less than or equal to 5"
   echo "Kubernetes resource scan has failed, scan score has to be greater than or equal to 5"
   exit 1;
-fi
+fi;
