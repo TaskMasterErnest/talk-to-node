0cd3c52a7f20d10dd874cb3f66a349ad6bc371d7
TaskMasterErnest <kluernest08@gmail.com>
2023-05-12 17:11:00 +0000

    added new stuff to get OpenAPI 3 docs

--- src/main/java/com/devsecops/WebSecurityConfig.java
+++ src/main/java/com/devsecops/WebSecurityConfig.java
@@ -12,7 +12,7 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        // http.csrf().disable();
         return http.build();
     }
 
