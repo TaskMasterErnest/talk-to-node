719f6c871005ff9cde51e8ebbe01deb53f220569
TaskMasterErnest <kluernest08@gmail.com>
2023-05-12 16:56:52 +0000

    added new stuff to get OpenAPI 3 docs

--- src/main/java/com/devsecops/NumericApplication.java
+++ src/main/java/com/devsecops/NumericApplication.java
@@ -3,7 +3,10 @@ package com.devsecops;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
 @SpringBootApplication
+@OpenAPIDefinition
 public class NumericApplication {
 
 	public static void main(String[] args) {
