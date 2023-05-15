515da40ecdcc17b50194178b62710978bb0461f0
TaskMasterErnest <kluernest08@gmail.com>
2023-05-15 16:49:56 +0000

    added Cache-Control to code to mitigate error in DAST stage

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 public class NumericController {
@@ -20,20 +24,24 @@ public class NumericController {
 	public class compare {
 
 		@GetMapping("/")
-		public String welcome() {
+		public String welcome(HttpServletResponse response) {
+			response.setHeader("Cache-Control", "public, max-age=3600");
 			return "Kubernetes DevSecOps";
 		}
 
+
 		@GetMapping("/compare/{value}")
-		public String compareToFifty(@PathVariable int value) {
+		public String compareToFifty(@PathVariable int value, HttpServletResponse response) {
+			response.setHeader("Cache-Control", "public, max-age=3600");
 			String message = "Could not determine comparison";
 			if (value > 50) {
-				message = "Greater than 50";
+					message = "Greater than 50";
 			} else {
-				message = "Smaller than or equal to 50";
+					message = "Smaller than or equal to 50";
 			}
 			return message;
 		}
+
 
 		@GetMapping("/increment/{value}")
 		public int increment(@PathVariable int value) {
@@ -41,8 +49,14 @@ public class NumericController {
 			String response = responseEntity.getBody();
 			logger.info("Value Received in Request - " + value);
 			logger.info("Node Service Response - " + response);
+			
+			// Set Cache-Control header
+			HttpServletResponse response1 = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getResponse();
+			response1.setHeader("Cache-Control", "public, max-age=3600");
+			
 			return Integer.parseInt(response);
 		}
+
 	}
 
 }
