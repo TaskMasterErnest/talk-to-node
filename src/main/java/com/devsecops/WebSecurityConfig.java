bfeb1485440ff793f5a15b6842f267abbc2fd728
TaskMasterErnest <kluernest08@gmail.com>
2023-05-15 17:07:12 +0000

    added ZAP rules to code to mitigate error in DAST stage

--- src/main/java/com/devsecops/WebSecurityConfig.java
+++ src/main/java/com/devsecops/WebSecurityConfig.java
@@ -1,9 +1,7 @@
 package com.devsecops;
 
-import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.CacheControl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
