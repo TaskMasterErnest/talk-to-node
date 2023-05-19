f8422c2da9e00c663fcccaaaa52c66553aa9b019
TaskMasterErnest <kluernest08@gmail.com>
2023-05-19 14:58:26 +0000

    added stages for Kubernetes in production

--- k8s-config/k8s-production-deployment.sh
+++ k8s-config/k8s-production-deployment.sh
@@ -5,17 +5,17 @@
 #first get the image to be used for the deployment
 sed -i "s#replace#${imageName}#g" k8s-config/k8s_production_deployment_service.yaml
 
-kubectl -n production get deploy ${deploymentName} > /dev/null
-exit_code=$?
+# kubectl -n production get deploy ${prodDeploymentName} > /dev/null
+# exit_code=$?
 
-if [[ "$exit_code" -ne 0 ]]; then
-  echo "deployment ${deploymentName} does NOT exist"
-  kubectl -n production apply -f k8s-config/k8s_production_deployment_service.yaml
-elsesss
-  #here we confirm if the deployment exists. we just change the image build used for the file
-  echo "deployment ${deploymentName} does exist"
-  echo "image name = ${imageName}"
-  kubectl -n production set image deploy ${deploymentName} ${containerName}=${imageName} --record=true
-fi
+# if [[ "$exit_code" -ne 0 ]]; then
+#   echo "deployment ${prodDeploymentName} does NOT exist"
+#   kubectl -n production apply -f k8s-config/k8s_production_deployment_service.yaml
+# elsesss
+#   #here we confirm if the deployment exists. we just change the image build used for the file
+#   echo "deployment ${prodDeploymentName} does exist"
+#   echo "image name = ${imageName}"
+#   kubectl -n production set image deploy ${prodDeploymentName} ${containerName}=${imageName} --record=true
+# fi
 
-# kubectl apply -n default -f k8s-config/k8s_production_deployment_service.yaml
+kubectl apply -n default -f k8s-config/k8s_production_deployment_service.yaml
